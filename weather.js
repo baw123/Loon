4f3cfa5037755b5abc70052effd803186785407e
baw123 <lsac@vip.qq.com>
2020-05-25 12:48:40 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,20 +1,19 @@
 const weaapi = "https://www.tianqiapi.com/api/?version=v6&appid=35997835&appsecret=A3L1SWwc&cityid=101070701"
-
 $httpClient.get(weaapi, function(error, response, data){
-    if (error){
-        console.log(error);
-        $done();                   
-    } else {
-        var obj = JSON.parse(data);
-        console.log(obj);
-        var city = "所在城市： " + obj.city;
-        var wea = "天气状况： " + obj.wea + "  当前" + obj.tem + "℃  " + obj.tem2 + "℃～" + obj.tem1 + "℃";
-        var air = "当前风力： " + obj.win + obj.win_speed + "  风速" + obj.win_meter + "\n空气指数： " + obj.air + "  " + obj.air_level;
-        let wmation = [city,wea,air];
-        $notification.post(wmation[0], wmation[1], wmation[2]);
-        $done();
+        if (error){
+            console.log(error);
+            $done();                   
+        } else {
+            var obj = JSON.parse(data);
+            console.log(obj);
+            var city = "所在城市： " + obj.city;
+            var wea = "天气状况： " + obj.wea + "  当前" + obj.tem + "℃  " + obj.tem2 + "℃～" + obj.tem1 + "℃";
+            var air = "当前风力： " + obj.win + obj.win_speed + "  风速" + obj.win_meter + "\n空气指数： " + obj.air + "  " + obj.air_level;
+            let wmation = [city,wea,air];
+            $notification.post(wmation[0], wmation[1], wmation[2]);
+            $done();
+        }
     }
-}
 );
 
 
